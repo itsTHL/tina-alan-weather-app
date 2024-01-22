e2cbc532c81022bed6ea7895d6e8c782901fec6d
Tina Huynh Le <huynhletina@gmail.com>
2024-01-22 16:11:14 +0100

    implement list component and store state in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+// import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 import { uid } from "uid";
 import Form from "./components/Form";
+import List from "./components/List";
 
 function App() {
-  const [activities, setActivities] = useState("");
+  const [activities, setActivities] = useLocalStorageState("activity", {
+    defaultValue: [],
+  });
   console.log(activities);
 
   function handleAddActivity(newActivity) {
-    setActivities([...activities, { ...newActivity, id: uid() }]);
+    setActivities([...activities, { ...newActivity, key: uid() }]);
   }
 
   return (
     <>
+      <List activities={activities} />
       <Form onAddActivity={handleAddActivity} />
     </>
   );
