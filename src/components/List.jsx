af1df9d0293516c01c01f25eef6651e1db065cc4
ALAN TONELLO <alantb2@gmail.com>
2024-01-23 13:05:58 +0100

    update styling

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,15 +3,19 @@ export default function List({ activities, weatherStatus, onDeleteActivity }) {
     <>
       <h4>
         {weatherStatus === true
-          ? "The weather is awesome ☀️ Go outside and:"
-          : "Bad weather outside 🌧 Here's what you can do now:"}
+          ? "The weather is awesome! Go outside and:"
+          : "Bad weather outside. Here's what you can do now:"}
       </h4>
       <ul className="list">
         {activities.map(({ key, name }) => (
           <li key={key}>
             {name}
-            <button type="button" onClick={() => onDeleteActivity(key)}>
-              X
+            <button
+              id="closeButton"
+              type="button"
+              onClick={() => onDeleteActivity(key)}
+            >
+              ✖️
             </button>
           </li>
         ))}
